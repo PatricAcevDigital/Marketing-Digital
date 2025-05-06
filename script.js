ca7a1b202f7c978751e73306a43e23c2c8218344
Patricio Acevedo Diaz <patricioacevedodiazuno@gmail.com>
2025-05-06 14:42:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Página cargada correctamente.");
+
+    // Manejo del formulario
+    document.getElementById("productoForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // Evita el envío automático
+        alert("Producto agregado correctamente.");
+    });
+
+    // Manejo de enlaces del menú de navegación
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach(link => {
+       
+    });
+});document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página cargada correctamente.");
     const links = document.querySelectorAll("nav ul li a");
     
     links.forEach(link => {
